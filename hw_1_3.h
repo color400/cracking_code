e6a2598b6281fee0e4c574af339dc18f225c1957
color400 <color400@gmail.com>
2014-03-07 15:33:54 +0800

    Update hw_1_3.h

--- hw_1_3.h
+++ hw_1_3.h
@@ -4,5 +4,10 @@
 #include <iostream>
 #include <string>
 
+void RemoveDuplicatedChar (std::string &strIn);
+void Replace( char* szIn, const char cFindInString, const char cReplace );
+int FindFirstNotMatch(const char* szIn, char cTarget);
+int GetLength(const char* szIn);
+int RemoveDupChar_cstyle ( char* szIn );
 
 #endif 
